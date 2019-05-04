9cc723deaae306da023c80b6c102a6bb2fdc39f4
BorgRect <code10learn@gmail.com>
2019-05-04 13:16:18 -0500

    Add 1D array of the board.

--- main_loop.py
+++ main_loop.py
@@ -1,6 +1,20 @@
 
+cols = 7
+rows = 6
+board_size = cols * rows
+board_list = [None] * ((cols * rows)*(board_size))
+
+current_turn = 2
+current_col = 6
+current_row = 0
+
+
+current_index = (current_turn * board_size) + (current_col * 6) + (current_row)
+
 
 
 
 if __name__ == "__main__":
-    pass
+
+    print(len(board_list))
+    print(current_index)
