1ad95337531f2328cc7cfe6e61a29b9a9064a72a
BorgRect <code10learn@gmail.com>
2019-05-04 21:36:41 -0500

    Add 1D array of the board.

--- main_loop.py
+++ main_loop.py
@@ -1,20 +1,11 @@
-
+import numpy as np
 cols = 7
 rows = 6
 board_size = cols * rows
-board_list = [None] * ((cols * rows)*(board_size))
 
-current_turn = 2
-current_col = 6
-current_row = 0
-
-
-current_index = (current_turn * board_size) + (current_col * 6) + (current_row)
-
-
-
+board_list_np = np.zeros(shape=(board_size, cols, rows),dtype=int)
 
 if __name__ == "__main__":
 
-    print(len(board_list))
-    print(current_index)
+    print(board_list_np[0, 2, 3])
+    print(board_list_np[0, 2, 2])
