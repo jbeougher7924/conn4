4cce169937e8bd13b7f5c10fb36d2097095d7a73
jbeougher7924 <code10learn@gmail.com>
2019-05-13 14:23:50 -0500

    Update main_loop.py

--- main_loop.py
+++ main_loop.py
@@ -48,41 +48,41 @@ def print_tokensUsed():
     print(token_used_cols_np)
 
 
-def checkWin(board_phase, player):
-    print("Turn: {}".format(board_phase))
+def checkWin(board, board_phase, player):
+    # print("Turn: {}".format(board_phase))
     for col in range(cols - 3):
         for row in range(3, rows):
-            spot1 = board_list_np[board_phase, col, row ]
-            spot2 = board_list_np[board_phase, col + 1, row - 1]
-            spot3 = board_list_np[board_phase, col + 2, row - 2]
-            spot4 = board_list_np[board_phase, col + 3, row - 3]
+            spot1 = board[board_phase, col, row ]
+            spot2 = board[board_phase, col + 1, row - 1]
+            spot3 = board[board_phase, col + 2, row - 2]
+            spot4 = board[board_phase, col + 3, row - 3]
             if spot1 == player and spot2 == player and spot3 == player and spot4 == player:
                 return True
 
     for col in range(cols - 3):
         for row in range(rows - 3):
-            spot1 = board_list_np[board_phase, col, row ]
-            spot2 = board_list_np[board_phase, col + 1, row + 1]
-            spot3 = board_list_np[board_phase, col + 2, row + 2]
-            spot4 = board_list_np[board_phase, col + 3, row + 3]
+            spot1 = board[board_phase, col, row ]
+            spot2 = board[board_phase, col + 1, row + 1]
+            spot3 = board[board_phase, col + 2, row + 2]
+            spot4 = board[board_phase, col + 3, row + 3]
             if spot1 == player and spot2 == player and spot3 == player and spot4 == player:
                 return True
 
     for col in range(cols):
         for row in range(rows - 3):
-            spot1 = board_list_np[board_phase, col, row ]
-            spot2 = board_list_np[board_phase, col, row + 1]
-            spot3 = board_list_np[board_phase, col, row + 2]
-            spot4 = board_list_np[board_phase, col, row + 3]
+            spot1 = board[board_phase, col, row ]
+            spot2 = board[board_phase, col, row + 1]
+            spot3 = board[board_phase, col, row + 2]
+            spot4 = board[board_phase, col, row + 3]
             if spot1 == player and spot2 == player and spot3 == player and spot4 == player:
                 return True
 
     for col in range(cols - 3):
         for row in range(rows):
-            spot1 = board_list_np[board_phase, col, row ]
-            spot2 = board_list_np[board_phase, col + 1, row]
-            spot3 = board_list_np[board_phase, col + 2, row]
-            spot4 = board_list_np[board_phase, col + 3, row]
+            spot1 = board[board_phase, col, row ]
+            spot2 = board[board_phase, col + 1, row]
+            spot3 = board[board_phase, col + 2, row]
+            spot4 = board[board_phase, col + 3, row]
             if spot1 == player and spot2 == player and spot3 == player and spot4 == player:
                 return True
     return False
@@ -95,8 +95,10 @@ def human_turn(player):
     while human_nan:
         human_move = int(input("Player: {} Enter number 1 to 7 \n".format(player))) - 1
 
-        if human_move > 0 and human_move < 7:
+        if human_move >= 0 and human_move < 7:
             human_nan = False
+        else:
+            print("Not a valid move.")
 
     while not place_token(board_phase_count, human_move, player):
         print_board(board_phase_count)
@@ -104,85 +106,27 @@ def human_turn(player):
         human_move = int(input("Player: {} Enter number 1 to 7 \n".format(player))) - 1
 
 
-def drop_piece(board, row, col, piece):
-    board[row][col] = piece
+def drop_piece(board, board_phase, row, col, piece):  # updated to use numpy/board_phase
+    board[board_phase, col, row] = piece
 
 
-def is_valid_location(board, col):
-    return board[rows - 1][col] == 0
+def is_valid_location(board, board_phase, col):  # updated to use numpy
+    return board[board_phase, col, rows - 1] == 0
 
 
-def get_next_open_row(board, col):
+def get_next_open_row(board, board_phase, col):  # updated to use board_phase
     for r in range(rows):
-        if board[r][col] == 0:
+        if board[board_phase, col, r] == 0:
             return r
 
-        def winning_move(board, piece):
-            # Check horizontal locations for win
-            for c in range(cols - 3):
-                for r in range(rows):
-                    if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
-                        c + 3] == piece:
-                        return True
 
-            # Check vertical locations for win
-            for c in range(cols):
-                for r in range(rows - 3):
-                    if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
-                        c] == piece:
-                        return True
-
-            # Check positively sloped diaganols
-            for c in range(cols - 3):
-                for r in range(rows - 3):
-                    if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and \
-                            board[r + 3][
-                                c + 3] == piece:
-                        return True
-
-            # Check negatively sloped diaganols
-            for c in range(cols - 3):
-                for r in range(3, rows):
-                    if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
-                            board[r - 3][
-                                c + 3] == piece:
-                        return True
-
-def get_valid_locations(board):
+def get_valid_locations(board, board_phase):  # updated to use numpy
     valid_locations = []
     for col in range(cols):
-        if is_valid_location(board, col):
+        if is_valid_location(board, board_phase, col):
             valid_locations.append(col)
     return valid_locations
 
-def winning_move(board, piece):
-    # Check horizontal locations for win
-    for c in range(cols - 3):
-        for r in range(rows):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
-                c + 3] == piece:
-                return True
-
-    # Check vertical locations for win
-    for c in range(cols):
-        for r in range(rows - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
-                c] == piece:
-                return True
-
-    # Check positively sloped diaganols
-    for c in range(cols - 3):
-        for r in range(rows - 3):
-            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
-                c + 3] == piece:
-                return True
-
-    # Check negatively sloped diaganols
-    for c in range(cols - 3):
-        for r in range(3, rows):
-            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
-                c + 3] == piece:
-                return True
 
 def evaluate_window(window, piece):
     score = 0
@@ -202,65 +146,71 @@ def evaluate_window(window, piece):
 
     return score
 
-def score_position(board, piece):
+
+def score_position(board, board_phase, piece):  # updated for board_phase
     score = 0
 
-    ## Score center column
-    center_array = [int(i) for i in list(board[:, cols // 2])]
+    # Score center column
+    center_array = [int(i) for i in list(board[board_phase, cols // 2, :])]
     center_count = center_array.count(piece)
     score += center_count * 3
 
-    ## Score Horizontal
+    print(center_array)
+    # Score Horizontal
     for r in range(rows):
-        row_array = [int(i) for i in list(board[r, :])]
+        row_array = [int(i) for i in list(board[board_phase, :, r])]
         for c in range(cols - 3):
             window = row_array[c:c + WINDOW_LENGTH]
             score += evaluate_window(window, piece)
 
-    ## Score Vertical
+    # Score Vertical
     for c in range(cols):
-        col_array = [int(i) for i in list(board[:, c])]
+        col_array = [int(i) for i in list(board[board_phase, c, :])]
         for r in range(rows - 3):
             window = col_array[r:r + WINDOW_LENGTH]
             score += evaluate_window(window, piece)
 
-    ## Score posiive sloped diagonal
+    # Score posiive sloped diagonal
     for r in range(rows - 3):
         for c in range(cols - 3):
-            window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
+            window = [board[board_phase, c + i, r + i] for i in range(WINDOW_LENGTH)]
             score += evaluate_window(window, piece)
 
     for r in range(rows - 3):
         for c in range(cols - 3):
-            window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
+            window = [board[board_phase, c + 1, r + 3 - i] for i in range(WINDOW_LENGTH)]
             score += evaluate_window(window, piece)
 
     return score
 
-def is_terminal_node(board):
-    return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
 
-def minimax(board, depth, alpha, beta, maximizingPlayer):
-    valid_locations = get_valid_locations(board)
-    is_terminal = is_terminal_node(board)
+def is_terminal_node(board, board_phase):
+    return checkWin(board, board_phase, PLAYER_PIECE) or checkWin(board, board_phase, AI_PIECE) or \
+            len(get_valid_locations(board, board_phase)) == 0  # could be a problem this line was connected
+
+
+def minimax(board, board_phase, depth, alpha, beta, maximizingPlayer):
+    valid_locations = get_valid_locations(board, board_phase)  # U NP
+    is_terminal = is_terminal_node(board, board_phase)  # U NP
     if depth == 0 or is_terminal:
         if is_terminal:
-            if winning_move(board, AI_PIECE):
-                return (None, 100000000000000)
-            elif winning_move(board, PLAYER_PIECE):
-                return (None, -10000000000000)
+            if checkWin(board, board_phase, AI_PIECE):  # changed to use checkWin and not original function
+                return None, 100000000000000
+            elif checkWin(board, board_phase, PLAYER_PIECE):  # changed to use checkWin and not original function
+                return None, -10000000000000
             else:  # Game is over, no more valid moves
-                return (None, 0)
+                return None, 0
         else:  # Depth is zero
-            return (None, score_position(board, AI_PIECE))
+            return None, score_position(board, board_phase, AI_PIECE)  # U NP
     if maximizingPlayer:
         value = -math.inf
         column = random.choice(valid_locations)
         for col in valid_locations:
-            row = get_next_open_row(board, col)
+            row = get_next_open_row(board, board_phase, col)
             b_copy = board.copy()
-            drop_piece(b_copy, row, col, AI_PIECE)
-            new_score = minimax(b_copy, depth - 1, alpha, beta, False)[1]
+            drop_piece(b_copy, board_phase, row, col, AI_PIECE)
+            # place_token(b_copy, board_phase, row, col, AI_PIECE)
+            new_score = minimax(b_copy, board_phase, depth - 1, alpha, beta, False)[1]
             if new_score > value:
                 value = new_score
                 column = col
@@ -273,10 +223,10 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
         value = math.inf
         column = random.choice(valid_locations)
         for col in valid_locations:
-            row = get_next_open_row(board, col)
+            row = get_next_open_row(board, board_phase, col)
             b_copy = board.copy()
-            drop_piece(b_copy, row, col, PLAYER_PIECE)
-            new_score = minimax(b_copy, depth - 1, alpha, beta, True)[1]
+            drop_piece(b_copy, board_phase, row, col, PLAYER_PIECE)
+            new_score = minimax(b_copy, board_phase, depth - 1, alpha, beta, True)[1]
             if new_score < value:
                 value = new_score
                 column = col
@@ -286,8 +236,13 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
         return column, value
 
 
-def ai_turn():
-    pass
+def ai_turn(board, board_phase):
+    col, minimax_score = minimax(board, board_phase, 5, -math.inf, math.inf, True)
+
+    if is_valid_location(board, board_phase, col):
+        row = get_next_open_row(board, board_phase, col)
+        # drop_piece(board,board_phase, row, col, AI_PIECE)
+        place_token(board_phase, col, AI_PIECE)
 
 
 def switch_player(player, game_status):
@@ -312,16 +267,16 @@ if __name__ == "__main__":
             if current_player == 1:
                 human_turn(current_player)
             else:
-                human_turn(current_player)
+                ai_turn(board_list_np, board_phase_count)
 
-            game_loop = checkWin(board_phase_count, current_player)
+            game_loop = checkWin(board_list_np, board_phase_count, current_player)
             current_player = switch_player(current_player, game_loop)
             board_phase_count += 1
 
     else:
         while not game_loop:
             human_turn(current_player)
-            game_loop = checkWin(board_phase_count, current_player)
+            game_loop = checkWin(board_list_np, board_phase_count, current_player)
             current_player = switch_player(current_player, game_loop)
             board_phase_count += 1
 
